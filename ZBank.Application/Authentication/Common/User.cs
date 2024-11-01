19cfc04e6ec1ccc33f22f757915df8948c1bbc47
skmkqw <tkorsakov77@gmail.com>
2024-11-01 19:09:54 +0100

    id for temporary user model

--- ZBank.Application/Authentication/Common/User.cs
+++ ZBank.Application/Authentication/Common/User.cs
@@ -1,3 +1,3 @@
 namespace ZBank.Application.Authentication.Common;
 
-public record User(string FirstName, string LastName, string Email, string Password);
+public record User(Guid Id, string FirstName, string LastName, string Email, string Password);
