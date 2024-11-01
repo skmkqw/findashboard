4aa898fd81897781d8f9e1f038ffaa37c31ec234
skmkqw <tkorsakov77@gmail.com>
2024-11-01 20:39:19 +0100

    User

---
+++ ZBank.Domain/UserAggregate/User.cs
@@ -0,0 +1,43 @@
+using ZBank.Domain.Common.Models;
+using ZBank.Domain.UserAggregate.ValueObjects;
+
+namespace ZBank.Domain.UserAggregate;
+
+public sealed class User : AggregateRoot<UserId>
+{
+    public string FirstName { get; }
+
+    public string LastName { get; }
+
+    public string Email { get; }
+
+    public string Password { get; }
+    
+    private User(
+        UserId id, 
+        string firstName, 
+        string lastName, 
+        string email, 
+        string password) : base(id)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        Email = email;
+        Password = password;
+    }
+
+    public static User Create(string firstName, string lastName, string email, string password)
+    {
+        return new User(
+            UserId.CreateUnique(), 
+            firstName, lastName,
+            email, 
+            password);
+    }
+    
+#pragma warning disable CS8618
+    private User()
+#pragma warning restore CS8618
+    {
+    }
+}
