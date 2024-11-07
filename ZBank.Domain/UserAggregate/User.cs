61c9f86ce14c26e7880d634d7c16066aa17d7fac
skmkqw <tkorsakov77@gmail.com>
2024-11-07 22:26:48 +0100

    extended UserAggregate with ProfileIds

--- ZBank.Domain/UserAggregate/User.cs
+++ ZBank.Domain/UserAggregate/User.cs
@@ -1,4 +1,5 @@
 using ZBank.Domain.Common.Models;
+using ZBank.Domain.ProfileAggregate.ValueObjects;
 using ZBank.Domain.TeamAggregate.ValueObjects;
 using ZBank.Domain.UserAggregate.ValueObjects;
 
@@ -16,7 +17,11 @@ public sealed class User : AggregateRoot<UserId>
     
     public IReadOnlyList<TeamId> TeamIds => _teamIds.AsReadOnly();
     
+    public IReadOnlyList<ProfileId> ProfileIds => _profileIds.AsReadOnly();
+    
     private readonly List<TeamId> _teamIds = new();
+    
+    private readonly List<ProfileId> _profileIds = new();
     
     private User(
         UserId id, 
