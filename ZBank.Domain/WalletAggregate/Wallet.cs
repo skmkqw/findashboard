ff02927d30ebf0e53689ec14abf3ec3f58414a37
skmkqw <tkorsakov77@gmail.com>
2024-11-07 22:24:36 +0100

    extended WalletAggregate with ProfileId

--- ZBank.Domain/WalletAggregate/Wallet.cs
+++ ZBank.Domain/WalletAggregate/Wallet.cs
@@ -1,4 +1,5 @@
 using ZBank.Domain.Common.Models;
+using ZBank.Domain.ProfileAggregate.ValueObjects;
 using ZBank.Domain.WalletAggregate.ValueObjects;
 
 namespace ZBank.Domain.WalletAggregate;
@@ -7,19 +8,21 @@ public class Wallet : AggregateRoot<WalletId>
 {
     public string Address { get; }
     
-    
     //TODO This is probably not how i want to implement it
     public string Type { get; }
 
-    private Wallet(string address, string type)
+    public ProfileId ProfileId { get; }
+
+    private Wallet(string address, string type, ProfileId profileId)
     {
         Address = address;
         Type = type;
+        ProfileId = profileId;
     }
 
-    public static Wallet Create(string address, string type)
+    public static Wallet Create(string address, string type, ProfileId profileId)
     {
-        return new Wallet(address, type);
+        return new Wallet(address, type, profileId);
     }
     
 #pragma warning disable CS8618
