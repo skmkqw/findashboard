4db8eae7c2505bc30adc474743bdb3d0ca7af987
skmkqw <tkorsakov77@gmail.com>
2024-11-07 22:41:33 +0100

    ActivityLog.cs

---
+++ ZBank.Domain/ActivityAggregate/Entities/ActivityLog.cs
@@ -0,0 +1,31 @@
+using ZBank.Domain.ActivityAggregate.ValueObjects;
+using ZBank.Domain.Common.Models;
+using ZBank.Domain.ProfileAggregate.ValueObjects;
+using ZBank.Domain.UserAggregate.ValueObjects;
+using ZBank.Domain.WalletAggregate.ValueObjects;
+
+namespace ZBank.Domain.ActivityAggregate.Entities;
+
+public class ActivityLog : Entity<ActivityLogId>
+{
+    public UserId UserId { get; }
+
+    public ProfileId ProfileId { get; }
+
+    public WalletId WalletId { get; }
+
+    public DateTime TimeStamp { get; }
+    
+    private ActivityLog(UserId userId, ProfileId profileId, WalletId walletId)
+    {
+        UserId = userId;
+        ProfileId = profileId;
+        WalletId = walletId;
+        TimeStamp = DateTime.UtcNow;
+    }
+
+    public static ActivityLog Create(UserId userId, ProfileId profileId, WalletId walletId)
+    {
+        return new ActivityLog(userId, profileId, walletId);
+    }
+}
