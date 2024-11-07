22c79b5c8bbf69fe0daa629a8f7c517a09c61322
skmkqw <tkorsakov77@gmail.com>
2024-11-07 22:57:20 +0100

    update profile & add/remove wallet methods

--- ZBank.Domain/ProfileAggregate/Profile.cs
+++ ZBank.Domain/ProfileAggregate/Profile.cs
@@ -1,19 +1,19 @@
 using ZBank.Domain.Common.Models;
 using ZBank.Domain.ProfileAggregate.ValueObjects;
 using ZBank.Domain.UserAggregate.ValueObjects;
-using ZBank.Domain.WalletAggregate;
+using ZBank.Domain.WalletAggregate.ValueObjects;
 
 namespace ZBank.Domain.ProfileAggregate;
 
 public class Profile : AggregateRoot<ProfileId>
 {
-    public string Name { get; }
+    public string Name { get; private set;  }
     
     public UserId OwnerId { get; }
     
-    public IReadOnlyList<Wallet> Wallets => _wallets;
+    public IReadOnlyList<WalletId> WalletIds => _walletIds;
     
-    private readonly List<Wallet> _wallets = new();
+    private readonly List<WalletId> _walletIds = new();
 
     private Profile(string name, UserId ownerId)
     {
@@ -26,6 +26,20 @@ public class Profile : AggregateRoot<ProfileId>
         return new Profile(name, ownerId);
     }
     
+    public void Update(string name)
+    {
+        Name = name;
+    }
+
+    public void AddWallet(WalletId walletId)
+    {
+        _walletIds.Add(walletId);
+    }
+
+    public void DeleteWallet(WalletId walletId)
+    {
+        _walletIds.Remove(walletId);
+    }
 #pragma warning disable CS8618
     private Profile()
 #pragma warning restore CS8618
