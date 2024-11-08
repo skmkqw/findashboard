40dad61365ab620484d36c2723fedfcdd154d507
skmkqw <tkorsakov77@gmail.com>
2024-11-08 17:24:29 +0100

    ProfileConfigurations.cs

---
+++ ZBank.Infrastructure/Persistance/Configurations/ProfileConfigurations.cs
@@ -0,0 +1,61 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ZBank.Domain.ProfileAggregate;
+using ZBank.Domain.ProfileAggregate.ValueObjects;
+
+namespace ZBank.Infrastructure.Persistance.Configurations;
+
+public class ProfileConfigurations : IEntityTypeConfiguration<Profile>
+{
+    public void Configure(EntityTypeBuilder<Profile> builder)
+    {
+        throw new NotImplementedException();
+    }
+    
+    public void ConfigureProfilesTable(EntityTypeBuilder<Profile> builder)
+    {
+        builder.ToTable("Profiles");
+        
+        //PK
+        builder.HasKey(x => x.Id);
+        
+        //ID
+        builder.Property(x => x.Id)
+            .ValueGeneratedOnAdd()
+            .HasConversion(
+                id => id.Value,
+                value => ProfileId.Create(value));
+        
+        //Name
+        builder.Property(x => x.Name)
+            .HasMaxLength(100);
+        
+        //FK
+        builder.HasOne(o => o.OwnerId)
+            .WithMany()
+            .HasForeignKey(o => o.OwnerId);
+    }
+
+    private void ConfigureWallerIdsTable(EntityTypeBuilder<Profile> builder)
+    {
+        builder.OwnsMany(r => r.WalletIds, wib =>
+        {
+            wib.ToTable("ProfileWalletIds");
+            
+            //FK
+            wib.WithOwner().HasForeignKey("ProfileId");
+            
+            //PK
+            wib.HasKey("Id");
+            
+            //ID
+            wib.Property(d => d.Value)
+                .ValueGeneratedNever()
+                .HasColumnName("WalletId");
+
+        });
+        
+        builder.Metadata.FindNavigation(nameof(Profile.WalletIds))!
+            .SetPropertyAccessMode(PropertyAccessMode.Field);
+    }
+}
