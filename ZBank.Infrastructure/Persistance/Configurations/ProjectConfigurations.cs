6cbe0c187cd56ba2628d410f01481d1ca7ac0d2a
skmkqw <tkorsakov77@gmail.com>
2024-11-08 17:34:19 +0100

    ProjectConfigurations.cs

---
+++ ZBank.Infrastructure/Persistance/Configurations/ProjectConfigurations.cs
@@ -0,0 +1,38 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ZBank.Domain.ProjectAggregate;
+using ZBank.Domain.ProjectAggregate.ValueObjects;
+
+namespace ZBank.Infrastructure.Persistance.Configurations;
+
+public class ProjectConfigurations : IEntityTypeConfiguration<Project>
+{
+    public void Configure(EntityTypeBuilder<Project> builder)
+    {
+        ConfigureProjectsTable(builder);
+    }
+    
+    public void ConfigureProjectsTable(EntityTypeBuilder<Project> builder)
+    {
+        builder.ToTable("Profiles");
+        
+        //PK
+        builder.HasKey(x => x.Id);
+        
+        //ID
+        builder.Property(x => x.Id)
+            .ValueGeneratedOnAdd()
+            .HasConversion(
+                id => id.Value,
+                value => ProjectId.Create(value));
+        
+        //Name
+        builder.Property(x => x.Name)
+            .HasMaxLength(100);
+        
+        //FK
+        builder.HasOne(o => o.TeamId)
+            .WithMany()
+            .HasForeignKey(o => o.TeamId);
+    }
+}
