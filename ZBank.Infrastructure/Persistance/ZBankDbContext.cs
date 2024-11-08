10f76b547f42aed2791bdc6cbfa3093232ffcf05
skmkqw <tkorsakov77@gmail.com>
2024-11-08 17:56:31 +0100

    updated ZBankDbContext.cs

--- ZBank.Infrastructure/Persistance/ZBankDbContext.cs
+++ ZBank.Infrastructure/Persistance/ZBankDbContext.cs
@@ -1,11 +1,26 @@
 using Microsoft.EntityFrameworkCore;
+using ZBank.Domain.ActivityAggregate;
+using ZBank.Domain.ProfileAggregate;
+using ZBank.Domain.ProjectAggregate;
+using ZBank.Domain.TeamAggregate;
 using ZBank.Domain.UserAggregate;
+using ZBank.Domain.WalletAggregate;
 
 namespace ZBank.Infrastructure.Persistance;
 
 public class ZBankDbContext : DbContext
 {
     public DbSet<User> Users { get; set; }
+    
+    public DbSet<Activity> Activities { get; set; }
+    
+    public DbSet<Profile> Profiles { get; set; }
+    
+    public DbSet<Project> Projects { get; set; }
+    
+    public DbSet<Team> Teams { get; set; }
+    
+    public DbSet<Wallet> Wallets { get; set; }
     
     public ZBankDbContext(DbContextOptions<ZBankDbContext> options) : base(options)
     {
