89920d0d80c315d10f52e2b5ee04ba527d342269
skmkqw <tkorsakov77@gmail.com>
2024-11-13 23:35:14 +0100

    adjusted mapping configs

--- ZBank.API/Mapping/TeamMappingConfigurations.cs
+++ ZBank.API/Mapping/TeamMappingConfigurations.cs
@@ -2,6 +2,7 @@ using Mapster;
 using ZBank.Application.Teams.Commands.CreateTeam;
 using ZBank.Contracts.Teams.CreateTeam;
 using ZBank.Domain.TeamAggregate;
+using ZBank.Domain.UserAggregate.ValueObjects;
 
 namespace ZBank.API.Mapping;
 
@@ -9,7 +10,9 @@ public class TeamMappingConfigurations : IRegister
 {
     public void Register(TypeAdapterConfig config)
     {
-        config.NewConfig<CreateTeamRequest, CreateTeamCommand>();
+        config.NewConfig<(CreateTeamRequest request, Guid? ownerId), CreateTeamCommand>()
+            .Map(dest => dest.OwnerId, src => UserId.Create(src.ownerId!.Value))
+            .Map(dest => dest, src => src.request);
 
         config.NewConfig<Team, CreateTeamResponse>()
             .Map(dest => dest.MemberIds, src => src.UserIds.Select(u => u.Value.ToString()).ToList());
