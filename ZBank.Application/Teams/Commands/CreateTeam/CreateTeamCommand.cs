c757d460cd75f370b1899a686975c9319dafaf7b
skmkqw <tkorsakov77@gmail.com>
2024-11-13 23:34:32 +0100

    extended CreateTeamCommand.cs

--- ZBank.Application/Teams/Commands/CreateTeam/CreateTeamCommand.cs
+++ ZBank.Application/Teams/Commands/CreateTeam/CreateTeamCommand.cs
@@ -1,7 +1,8 @@
 using ErrorOr;
 using MediatR;
 using ZBank.Domain.TeamAggregate;
+using ZBank.Domain.UserAggregate.ValueObjects;
 
 namespace ZBank.Application.Teams.Commands.CreateTeam;
 
-public record CreateTeamCommand(string Name, string? Description, List<string> MemberEmails) : IRequest<ErrorOr<Team>>;
+public record CreateTeamCommand(string Name, string? Description, List<string> MemberEmails, UserId OwnerId) : IRequest<ErrorOr<Team>>;
