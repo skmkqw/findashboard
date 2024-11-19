60d988705c6406e5436191f71450cf8f5c20eac0
skmkqw <tkorsakov77@gmail.com>
2024-11-19 17:02:00 +0100

    adjusted SendInviteRequest.cs

--- ZBank.Contracts/Teams/SendInvite/SendInviteRequest.cs
+++ ZBank.Contracts/Teams/SendInvite/SendInviteRequest.cs
@@ -1,3 +1,3 @@
 namespace ZBank.Contracts.Teams.SendInvite;
 
-public record SendInviteRequest(Guid SenderId, string SenderFullName, string ReceiverEmail, Guid TeamId, string TeamName);
+public record SendInviteRequest(string SenderFullName, string ReceiverEmail, Guid TeamId, string TeamName);
