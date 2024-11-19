8a58c9d96ff299fc0f52eee129cd1870a28bd620
skmkqw <tkorsakov77@gmail.com>
2024-11-19 22:04:53 +0100

    new notification domain errors

--- ZBank.Domain/Common/Errors/Errors.Notification.cs
+++ ZBank.Domain/Common/Errors/Errors.Notification.cs
@@ -1,4 +1,5 @@
 using ErrorOr;
+using ZBank.Domain.NotificationAggregate.ValueObjects;
 
 namespace ZBank.Domain.Common.Errors;
 
@@ -8,6 +9,13 @@ public static partial class Errors
     {
         public static class TeamInvite
         {
+            public static Error AccessDenied => Error.Forbidden(
+                code: "Notification.TeamInvite.AccessDenied",
+                description: "You are not allowed to accept/decline this request");
+            public static Error TeamInviteNotFound(NotificationId notificationId) => Error.NotFound(
+                code: "Notification.TeamInvite.NotFound",
+                description: $"Join team request with id '{notificationId.Value.ToString()}' not found or doesn't exist.");
+            
             public static Error TeamInviteAlreadyExists(string receiverEmail, string teamName) => Error.Conflict(
                 code: "Notification.TeamInvite.AlreadyExists", 
                 description: $"'{receiverEmail}' is already invited to team '{teamName}'.");   
