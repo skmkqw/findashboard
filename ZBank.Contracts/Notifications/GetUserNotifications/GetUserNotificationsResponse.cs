900956ab735e2d0490cc97988656f295fd3c545c
skmkqw <tkorsakov77@gmail.com>
2024-11-21 21:56:53 +0100

    added CreatedDateTime to GetUserNotificationsResponse.cs

--- ZBank.Contracts/Notifications/GetUserNotifications/GetUserNotificationsResponse.cs
+++ ZBank.Contracts/Notifications/GetUserNotifications/GetUserNotificationsResponse.cs
@@ -4,8 +4,8 @@ public record NotificationSenderResponse(string SenderId, string SenderFullName)
 
 public record NotificationTeamResponse(string TeamId, string TeamName);
 
-public record InformationNotificationResponse(string Content, bool IsRead, string ReceiverId, NotificationSenderResponse NotificationSender);
+public record InformationNotificationResponse(string Content, bool IsRead, string ReceiverId, DateTime CreatedDateTime, NotificationSenderResponse NotificationSender);
 
-public record TeamInviteNotificationResponse(string Content, bool IsRead, string ReceiverId, NotificationSenderResponse NotificationSender, NotificationTeamResponse Team);
+public record TeamInviteNotificationResponse(string Content, bool IsRead, string ReceiverId, DateTime CreatedDateTime, NotificationSenderResponse NotificationSender, NotificationTeamResponse Team);
 
 public record GetUserNotificationsResponse(List<InformationNotificationResponse> InformationNotifications, List<TeamInviteNotificationResponse> TeamInviteNotifications);
