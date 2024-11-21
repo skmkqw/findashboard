618b271b2e4cf912ffc2e3a2d5e6ab68b9ec9611
skmkqw <tkorsakov77@gmail.com>
2024-11-21 23:20:53 +0100

    InformationNotification domain errors

--- ZBank.Domain/Common/Errors/Errors.Notification.cs
+++ ZBank.Domain/Common/Errors/Errors.Notification.cs
@@ -7,12 +7,26 @@ public static partial class Errors
 {
     public static class Notification
     {
+        public static class InformationNotification
+        {
+            public static Error AccessDenied => Error.Unauthorized(
+                code: "Notification.InformationNotification.AccessDenied",
+                description: "You are not allowed to modify this notification.");
+            public static Error NotFound(NotificationId notificationId) => Error.NotFound(
+                code: "Notification.InformationNotification.NotFound",
+                description: $"Informational notification with id '{notificationId.Value.ToString()}' not found or doesn't exist.");
+            
+            public static Error IsAlreadyRead(NotificationId notificationId) => Error.Conflict(
+                code: "Notification.InformationNotification.IsAlreadyRead",
+                description: $"Informational notification with id '{notificationId.Value.ToString()}' is already marked as read.");
+        }
+        
         public static class TeamInvite
         {
             public static Error AccessDenied => Error.Unauthorized(
                 code: "Notification.TeamInvite.AccessDenied",
                 description: "You are not allowed to accept/decline this request");
-            public static Error TeamInviteNotFound(NotificationId notificationId) => Error.NotFound(
+            public static Error NotFound(NotificationId notificationId) => Error.NotFound(
                 code: "Notification.TeamInvite.NotFound",
                 description: $"Join team request with id '{notificationId.Value.ToString()}' not found or doesn't exist.");
             
