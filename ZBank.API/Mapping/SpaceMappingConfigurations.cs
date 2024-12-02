20d1d34a0b8d16de726b6fdd7c2919c6d5afd982
skmkqw <tkorsakov77@gmail.com>
2024-12-02 19:56:52 +0100

    new mapping configs

--- ZBank.API/Mapping/SpaceMappingConfigurations.cs
+++ ZBank.API/Mapping/SpaceMappingConfigurations.cs
@@ -1,6 +1,8 @@
 using Mapster;
 using ZBank.Application.Spaces.Commands.CreateSpace;
+using ZBank.Application.Spaces.Queries.GetSpace;
 using ZBank.Contracts.Spaces.CreateSpace;
+using ZBank.Contracts.Spaces.GetSpace;
 using ZBank.Domain.TeamAggregate;
 using ZBank.Domain.UserAggregate.ValueObjects;
 
@@ -17,6 +19,16 @@ public class SpaceMappingConfigurations : IRegister
         config.NewConfig<PersonalSpace, CreateSpaceResponse>()
             .Map(dest => dest.Id, src => src.Id.Value)
             .Map(dest => dest.OwnerId, src => src.OwnerId.Value)
+            .Map(dest => dest, src => src);
+
+        config.NewConfig<Guid?, GetSpaceQuery>()
+            .Map(dest => dest.OwnerId, src => UserId.Create(src!.Value));
+        
+        config.NewConfig<PersonalSpace, GetSpaceResponse>()
+            .Map(dest => dest.Id, src => src.Id.Value)
+            .Map(dest => dest.OwnerId, src => src.OwnerId.Value)
+            .Map(dest => dest.ProjectIds, src => src.ProjectIds.Select(u => u.Value).ToList())
+            .Map(dest => dest.ActivityIds, src => src.ActivityIds.Select(u => u.Value).ToList())
             .Map(dest => dest, src => src);
     }
 }
