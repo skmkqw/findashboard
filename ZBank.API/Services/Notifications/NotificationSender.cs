aeacdb5d171b075d725dc2c255bbd9adea858da1
skmkqw <tkorsakov77@gmail.com>
2024-12-16 18:35:34 +0100

    NotificationSender.cs

---
+++ ZBank.API/Services/Notifications/NotificationSender.cs
@@ -0,0 +1,35 @@
+using Microsoft.AspNetCore.SignalR;
+using ZBank.API.Hubs;
+using ZBank.API.Interfaces;
+using ZBank.Application.Common.Interfaces.Services;
+using ZBank.Domain.NotificationAggregate;
+
+namespace ZBank.API.Services.Notifications;
+
+public class NotificationSender<T> : INotificationSender where T : Hub<INotificationClient>
+{
+    private readonly IUserConnectionManager _connectionManager;
+
+    private readonly IHubContext<NotificationHub, INotificationClient> _notificationHubContext;
+
+    public NotificationSender(IUserConnectionManager connectionManager, IHubContext<NotificationHub, INotificationClient> notificationHubContext)
+    {
+        _connectionManager = connectionManager;
+        _notificationHubContext = notificationHubContext;
+    }
+    
+    public async Task SendInformationNotification(InformationNotification notification)
+    {
+        var receiverId = notification.NotificationReceiverId;
+        var connections = _connectionManager.GetConnections(receiverId);
+
+        if (connections != null)
+        {
+            foreach (var connectionId in connections)
+            {
+                await _notificationHubContext.Clients.Client(connectionId)
+                    .ReceiveInformationNotification(notification);
+            }
+        }
+    }
+}
