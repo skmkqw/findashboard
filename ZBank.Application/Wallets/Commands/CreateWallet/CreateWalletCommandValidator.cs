5edb6ccd2a987fb6b7b2bb40089e6e2c26fb3437
skmkqw <tkorsakov77@gmail.com>
2024-12-18 19:02:17 +0100

    CreateWalletCommandValidator.cs

---
+++ ZBank.Application/Wallets/Commands/CreateWallet/CreateWalletCommandValidator.cs
@@ -0,0 +1,22 @@
+using FluentValidation;
+
+namespace ZBank.Application.Wallets.Commands.CreateWallet;
+
+public class CreateWalletCommandValidator : AbstractValidator<CreateWalletCommand>
+{
+    public CreateWalletCommandValidator()
+    {
+        RuleFor(x => x.Address)
+            .NotEmpty().WithMessage("Address is required")
+            .Length(20, 200).WithMessage("Address must be between 20 and 200 characters long");
+        
+        RuleFor(x => x.Type)
+            .NotEmpty().WithMessage("Type is required");
+        
+        RuleFor(x => x.UserId.Value)
+            .NotEmpty().WithMessage("User ID is required");
+        
+        RuleFor(x => x.ProfileId.Value)
+            .NotEmpty().WithMessage("Profile ID is required");
+    }
+}
