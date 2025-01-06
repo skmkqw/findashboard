72a8760179c801e0c8e639889f27234b96046454
skmkqw <tkorsakov77@gmail.com>
2025-01-06 20:45:57 +0100

    new mapping configs

--- ZBank.API/Mapping/WalletMappingConfigurations.cs
+++ ZBank.API/Mapping/WalletMappingConfigurations.cs
@@ -1,9 +1,14 @@
 using Mapster;
+using ZBank.Application.Wallets.Commands.AddBalance;
 using ZBank.Application.Wallets.Commands.CreateWallet;
+using ZBank.Contracts.Wallets.AddBalance;
 using ZBank.Contracts.Wallets.CreateWallet;
+using ZBank.Domain.CurrencyAggregate.ValueObjects;
 using ZBank.Domain.ProfileAggregate.ValueObjects;
 using ZBank.Domain.UserAggregate.ValueObjects;
 using ZBank.Domain.WalletAggregate;
+using ZBank.Domain.WalletAggregate.Entities;
+using ZBank.Domain.WalletAggregate.ValueObjects;
 
 namespace ZBank.API.Mapping;
 
@@ -19,5 +24,15 @@ public class WalletMappingConfigurations : IRegister
         config.NewConfig<Wallet, CreateWalletResponse>()
             .Map(dest => dest.Id, src => src.Id.Value)
             .Map(dest => dest.ProfileId, src => src.ProfileId.Value);
+
+        config.NewConfig<(AddBalanceRequest request, Guid? ownerId), AddBalanceCommand>()
+            .Map(dest => dest.UserId, src => UserId.Create(src.ownerId!.Value))
+            .Map(dest => dest.CurrencyId, src => CurrencyId.Create(src.request.CurrencyId))
+            .Map(dest => dest.WalletId, src => WalletId.Create(src.request.WalletId))
+            .Map(dest => dest.Amount, src => src.request.Amount);
+
+        config.NewConfig<Balance, AddBalanceResponse>()
+            .Map(dest => dest.Id, src => src.Id.Value)
+            .Map(dest => dest.CurrencyId, src => src.CurrencyId.Value);
     }
 }
