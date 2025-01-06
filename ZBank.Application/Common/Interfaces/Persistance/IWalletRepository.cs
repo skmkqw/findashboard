b6b5df8d5232e032910e6b82e1061601ee1fae72
skmkqw <tkorsakov77@gmail.com>
2025-01-06 20:45:30 +0100

    get wallet by id repository method

--- ZBank.Application/Common/Interfaces/Persistance/IWalletRepository.cs
+++ ZBank.Application/Common/Interfaces/Persistance/IWalletRepository.cs
@@ -1,8 +1,10 @@
 using ZBank.Domain.WalletAggregate;
+using ZBank.Domain.WalletAggregate.ValueObjects;
 
 namespace ZBank.Application.Common.Interfaces.Persistance;
 
 public interface IWalletRepository
 {
+    Task<Wallet?> GetById(WalletId id);
     void Add(Wallet wallet);
 }
--- ZBank.Infrastructure/Persistance/Repositories/WalletRepository.cs
+++ ZBank.Infrastructure/Persistance/Repositories/WalletRepository.cs
@@ -1,5 +1,6 @@
 using ZBank.Application.Common.Interfaces.Persistance;
 using ZBank.Domain.WalletAggregate;
+using ZBank.Domain.WalletAggregate.ValueObjects;
 
 namespace ZBank.Infrastructure.Persistance.Repositories;
 
@@ -11,7 +12,12 @@ public class WalletRepository : IWalletRepository
     {
         _dbContext = dbContext;
     }
-    
+
+    public async Task<Wallet?> GetById(WalletId id)
+    {
+        return await _dbContext.Wallets.FindAsync(id);
+    }
+
     public void Add(Wallet wallet)
     {
         _dbContext.Wallets.Add(wallet);
