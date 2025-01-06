b894a184171a80721a10d420f4d1d1a5f9ff74dc
skmkqw <tkorsakov77@gmail.com>
2025-01-06 17:44:24 +0100

    Currency aggregate

---
+++ ZBank.Domain/CurrencyAggregate/Currency.cs
@@ -0,0 +1,28 @@
+using ZBank.Domain.Common.Models;
+using ZBank.Domain.CurrencyAggregate.ValueObjects;
+
+namespace ZBank.Domain.CurrencyAggregate;
+
+public class Currency : AggregateRoot<CurrencyId>
+{
+    public string Symbol { get; private set; }
+    
+    public decimal Price { get; private set; }
+
+    private Currency(CurrencyId id, string symbol, decimal price) : base(id) 
+    {
+        Symbol = symbol;
+        Price = price;
+    }
+
+    public static Currency Create(string symbol, decimal price)
+    {
+        return new Currency(CurrencyId.CreateUnique(), symbol, price);
+    }
+    
+#pragma warning disable CS8618
+    private Currency()
+#pragma warning restore CS8618
+    {
+    }
+}
