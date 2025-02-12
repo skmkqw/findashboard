6314031c74c9df0276fbbd5fed370e67f1816e7b
skmkqw <tkorsakov77@gmail.com>
2025-02-12 23:13:38 +0100

    refactored GetUserNotificationsQueryHandler

--- ZBank.Application/Notifications/Queries/GetUserNotifications/GetUserNotificationsQueryHandler.cs
+++ ZBank.Application/Notifications/Queries/GetUserNotifications/GetUserNotificationsQueryHandler.cs
@@ -6,6 +6,7 @@ using ZBank.Application.Common.Interfaces.Persistance;
 using ZBank.Domain.Common.Errors;
 using ZBank.Domain.Common.Models;
 using ZBank.Domain.NotificationAggregate;
+using ZBank.Domain.UserAggregate.ValueObjects;
 
 namespace ZBank.Application.Notifications.Queries.GetUserNotifications;
 
@@ -13,50 +14,55 @@ public class GetUserNotificationsQueryHandler : IRequestHandler<GetUserNotificat
 {
     private readonly INotificationRepository _notificationRepository;
     
-    private readonly IUserRepository _userRepository;
-    
     private readonly ILogger<GetUserNotificationsQueryHandler> _logger;
-
-
-    public GetUserNotificationsQueryHandler(INotificationRepository notificationRepository, IUserRepository userRepository, ILogger<GetUserNotificationsQueryHandler> logger)
+    
+    public GetUserNotificationsQueryHandler(INotificationRepository notificationRepository, ILogger<GetUserNotificationsQueryHandler> logger)
     {
         _notificationRepository = notificationRepository;
         _logger = logger;
-        _userRepository = userRepository;
     }
 
     public async Task<ErrorOr<Dictionary<string, List<Notification>>>> Handle(GetUserNotificationsQuery request, CancellationToken cancellationToken)
     {
         _logger.LogInformation("Processing get notifications query for user with id: {UserId}", request.UserId.Value);
         
-        var user = await _userRepository.FindByIdAsync(request.UserId);
-
-        if (user is null)
-        {
-            _logger.LogInformation("User with ID: {Id} not found", request.UserId.Value);
-            return Errors.User.IdNotFound(request.UserId);
-        }
+        var getNotificationTypesResult = GetNotificationTypesFromAssembly();
         
-        var assembly = Assembly.GetAssembly(typeof(Notification));
+        if (getNotificationTypesResult.IsError)
+            return getNotificationTypesResult.Errors;
 
-        if (assembly is null)
-        {
-            _logger.LogWarning("Assembly not found");
-            return Error.Unexpected("Unexpected.AssemblyNotFound", "Assembly not found");
-        }
-            
-        var notificationTypes = assembly.GetTypes().Where(t => t.IsClass && !t.IsAbstract && t.IsSubclassOf(typeof(Notification)));
+        var notifications = await GetUserNotificationsAsync(request.UserId, getNotificationTypesResult.Value);
         
+        _logger.LogInformation("Successfully found {Count} notifications", notifications.Count);
+        return notifications;
+    }
+
+    private async Task<Dictionary<string, List<Notification>>> GetUserNotificationsAsync(UserId userId, IEnumerable<Type> notificationTypes)
+    {
         var result = new Dictionary<string, List<Notification>>();
         
-        var notifications = await _notificationRepository.FindUserNotifications(user.Id);
+        var notifications = await _notificationRepository.FindUserNotifications(userId);
 
         foreach (var notificationType in notificationTypes)
         {
             result[notificationType.Name] = notifications.Where(n => n.GetType() == notificationType).ToList();
         }
         
-        _logger.LogInformation("Successfully found {Count} notifications", notifications.Count);
         return result;
+    }
+
+    private ErrorOr<IEnumerable<Type>> GetNotificationTypesFromAssembly()
+    {
+        var assembly = Assembly.GetAssembly(typeof(Notification));
+
+        if (assembly is null)
+        {
+            _logger.LogError("Assembly not found");
+            return Error.Unexpected("Unexpected.AssemblyNotFound", "Assembly not found");
+        }
+            
+        return assembly.GetTypes()
+            .Where(t => t.IsClass && !t.IsAbstract && t.IsSubclassOf(typeof(Notification)))
+            .ToList();
     }
 }
