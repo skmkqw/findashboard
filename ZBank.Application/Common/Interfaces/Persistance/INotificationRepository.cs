c4749a2d309897c5d0621bc1cc311c3129d32aa9
skmkqw <tkorsakov77@gmail.com>
2025-02-12 22:26:59 +0100

    add FindNotificationWithSenderById & FindNotificationWithReceiverById methods to NotificationRepository

--- ZBank.Application/Common/Interfaces/Persistance/INotificationRepository.cs
+++ ZBank.Application/Common/Interfaces/Persistance/INotificationRepository.cs
@@ -1,3 +1,4 @@
+using ZBank.Application.Notifications.Common;
 using ZBank.Domain.Common.Models;
 using ZBank.Domain.NotificationAggregate;
 using ZBank.Domain.NotificationAggregate.ValueObjects;
@@ -9,6 +10,10 @@ namespace ZBank.Application.Common.Interfaces.Persistance;
 public interface INotificationRepository
 {
     Task<T?> FindNotificationById<T>(NotificationId id) where T : Notification;
+    
+    Task<NotificationWithUserDetails<T>?> FindNotificationWithSenderById<T>(NotificationId notificationId) where T : Notification;
+    
+    Task<NotificationWithUserDetails<T>?> FindNotificationWithReceiverById<T>(NotificationId notificationId) where T : Notification;
     
     Task<List<Notification>> FindUserNotifications(UserId userId);
     
--- ZBank.Infrastructure/Persistance/Repositories/NotificationRepository.cs
+++ ZBank.Infrastructure/Persistance/Repositories/NotificationRepository.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using ZBank.Application.Common.Interfaces.Persistance;
+using ZBank.Application.Notifications.Common;
 using ZBank.Domain.Common.Models;
 using ZBank.Domain.NotificationAggregate;
 using ZBank.Domain.NotificationAggregate.ValueObjects;
@@ -21,7 +22,37 @@ public class NotificationRepository : INotificationRepository
     {
         return await _dbContext.Notifications.OfType<T>().FirstOrDefaultAsync(n => n.Id == id);
     }
-    
+
+    public async Task<NotificationWithUserDetails<T>?> FindNotificationWithSenderById<T>(NotificationId notificationId) where T : Notification
+    {
+        var query = _dbContext.Notifications
+            .OfType<T>()
+            .Where(notification => notification.Id == notificationId)
+            .Join(
+                _dbContext.Users,
+                notification => notification.NotificationSender.SenderId,
+                user => user.Id,
+                (notification, user) => new NotificationWithUserDetails<T>(notification, user, UserRoles.Sender)
+            );
+
+        return await query.FirstOrDefaultAsync();
+    }
+
+    public async Task<NotificationWithUserDetails<T>?> FindNotificationWithReceiverById<T>(NotificationId notificationId) where T : Notification
+    {
+        var query = _dbContext.Notifications
+            .OfType<T>()
+            .Where(notification => notification.Id == notificationId)
+            .Join(
+                _dbContext.Users,
+                notification => notification.NotificationReceiverId,
+                user => user.Id,
+                (notification, user) => new NotificationWithUserDetails<T>(notification, user, UserRoles.Receiver)
+            );
+
+        return await query.FirstOrDefaultAsync();
+    }
+
     public async Task<List<Notification>> FindUserNotifications(UserId userId)
     {
         return await _dbContext.Notifications
