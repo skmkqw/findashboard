3046c198d3f829cff96e068ac6928892fc283e60
skmkqw <tkorsakov77@gmail.com>
2025-02-12 22:25:21 +0100

    add NotificationWithUserDetails common class

---
+++ ZBank.Application/Notifications/Common/NotificationWithUserDetails.cs
@@ -0,0 +1,38 @@
+using ZBank.Domain.Common.Models;
+using ZBank.Domain.NotificationAggregate.ValueObjects;
+using ZBank.Domain.UserAggregate;
+using ZBank.Domain.UserAggregate.ValueObjects;
+
+namespace ZBank.Application.Notifications.Common;
+
+public enum UserRoles
+{
+    Sender,
+    Receiver
+} 
+
+public class NotificationWithUserDetails<T> where T : Notification
+{
+    private readonly T _notification;
+    
+    private readonly User _user;
+    
+    public NotificationWithUserDetails(T notification, User user, UserRoles role)
+    {
+        _notification = notification;
+        _user = user;
+        Role = role;
+    }
+    
+    public UserRoles Role { get; init; }
+
+    public NotificationId NotificationId => _notification.Id;
+    
+    public bool IsRead => _notification.IsRead;
+    
+    public UserId ReceiverId => _notification.NotificationReceiverId;
+    
+    public UserId SenderId => _notification.NotificationSender.SenderId;
+    
+    public (T Notification, User User) GetEntities() => (_notification, _user);
+}
