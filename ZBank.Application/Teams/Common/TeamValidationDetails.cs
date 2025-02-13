f22636c8b5de1aabb692ac63b4648cfb2ba44542
skmkqw <tkorsakov77@gmail.com>
2025-02-13 21:51:22 +0100

    add TeamValidationDetails common model

---
+++ ZBank.Application/Teams/Common/TeamValidationDetails.cs
@@ -0,0 +1,27 @@
+using ZBank.Domain.Common.Models;
+using ZBank.Domain.TeamAggregate;
+using ZBank.Domain.TeamAggregate.ValueObjects;
+using ZBank.Domain.UserAggregate;
+
+namespace ZBank.Application.Teams.Common;
+
+public record TeamValidationDetails
+{
+    private readonly TeamBase _teamOrSpace;
+    
+    private readonly User _member;
+
+    public TeamValidationDetails(TeamBase teamOrSpace, User member)
+    {
+        _teamOrSpace = teamOrSpace;
+        _member = member;
+    }
+
+    public bool IsTeam => _teamOrSpace is Team;
+    
+    public bool HasAccess => !IsTeam || ((_teamOrSpace as Team)?.UserIds.Contains(_member.Id) ?? false);
+    
+    public TeamId TeamId => _teamOrSpace.Id;
+    
+    public (TeamBase Team, User Member) GetEntities() => (_teamOrSpace, _member);
+}
