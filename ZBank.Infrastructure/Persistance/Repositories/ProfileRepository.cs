5615021e76fe0a13d4a0aa2a6a05242f84b3d686
skmkqw <tkorsakov77@gmail.com>
2025-02-16 18:38:27 +0100

    add GetProfileWithOwnerAsync method for ProfileRepository

--- ZBank.Infrastructure/Persistance/Repositories/ProfileRepository.cs
+++ ZBank.Infrastructure/Persistance/Repositories/ProfileRepository.cs
@@ -1,7 +1,8 @@
+using Microsoft.EntityFrameworkCore;
 using ZBank.Application.Common.Interfaces.Persistance;
+using ZBank.Application.Common.Models.Validation;
 using ZBank.Domain.ProfileAggregate;
 using ZBank.Domain.ProfileAggregate.ValueObjects;
-using ZBank.Domain.UserAggregate.ValueObjects;
 
 namespace ZBank.Infrastructure.Persistance.Repositories;
 
@@ -17,6 +18,20 @@ public class ProfileRepository : IProfileRepository
     public async Task<Profile?> GetByIdAsync(ProfileId id)
     {
         return await _dbContext.Profiles.FindAsync(id); 
+    }
+
+    public async Task<ProfileValidationDetails?> GetProfileWithOwnerAsync(ProfileId profileId)
+    {
+        var query = _dbContext.Profiles
+            .Where(profile => profile.Id == profileId)
+            .Join(
+                _dbContext.Users,
+                profile => profile.OwnerId,
+                user => user.Id,
+                (profile, user) => new ProfileValidationDetails(profile, user)
+            );
+
+        return await query.FirstOrDefaultAsync();
     }
 
     public void Add(Profile profile)
