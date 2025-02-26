4366db93fe9f017289705557a0607ea67cd0e645
skmkqw <tkorsakov77@gmail.com>
2025-02-26 01:50:48 +0100

    removed CreateUnique method from IEntityId

--- ZBank.Domain/Common/Models/IEntityId.cs
+++ ZBank.Domain/Common/Models/IEntityId.cs
@@ -3,8 +3,6 @@ namespace ZBank.Domain.Common.Models;
 public interface IEntityId<T, TValue> where T : IEntityId<T, TValue>
 {
     TValue Value { get; }
-
-    static abstract T CreateUnique();
     
     static abstract T Create(TValue value);
 }
