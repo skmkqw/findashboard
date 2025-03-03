6de6e85d6437dde645f509c8abb6bc410d1e3af7
skmkqw <tkorsakov77@gmail.com>
2025-03-03 14:41:27 +0100

    add PriceChangeIn24Hours to Currency

--- ZBank.Domain/CurrencyAggregate/Currency.cs
+++ ZBank.Domain/CurrencyAggregate/Currency.cs
@@ -7,15 +7,18 @@ public class Currency : AggregateRoot<CurrencyId>
 {
     public decimal Price { get; private set; }
 
-    private Currency(CurrencyId symbol, decimal price) : base(symbol) 
+    public decimal PriceChangeIn24Hours { get; private set; }
+
+    private Currency(CurrencyId symbol, decimal price, decimal openPrice) : base(symbol) 
     {
         Id = symbol;
         Price = price;
+        PriceChangeIn24Hours = Math.Round((price - openPrice) / openPrice * 100, 2);
     }
 
-    public static Currency Create(string symbol, decimal price)
+    public static Currency Create(string symbol, decimal price, decimal openPrice)
     {
-        return new Currency(CurrencyId.Create(symbol), price);
+        return new Currency(CurrencyId.Create(symbol), price, openPrice);
     }
 
     public void UpdatePrice(decimal price)
