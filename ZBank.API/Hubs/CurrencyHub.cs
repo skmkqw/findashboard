12e3bfeeb7c7989d23a216c3bf00ea1208eed356
skmkqw <tkorsakov77@gmail.com>
2025-03-03 22:09:50 +0100

    add JoinTeamGroup & LeaveTeamGroup methods to CurrencyHub

--- ZBank.API/Hubs/CurrencyHub.cs
+++ ZBank.API/Hubs/CurrencyHub.cs
@@ -24,6 +24,40 @@ public class CurrencyHub : Hub<ICurrencyClient>
         _connectionManager = connectionManager;
     }
     
+    public async Task JoinTeamGroup(string teamId)
+    {
+        var userId = Context.User?.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+        if (userId != null && Guid.TryParse(userId, out var validUserId))
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, $"team-{teamId}");
+            _connectionManager.SetActiveTeam(UserId.Create(validUserId), teamId);
+            await Clients.Caller.ReceiveMessage($"Joined team group successfully. Group name: team-{teamId}. User ID: {validUserId}");
+            
+            var currentTeam = _connectionManager.GetActiveTeam(UserId.Create(validUserId));
+            if (currentTeam != null)
+            {
+                await Groups.RemoveFromGroupAsync(Context.ConnectionId, $"team-{currentTeam}");
+            }
+
+            await Groups.AddToGroupAsync(Context.ConnectionId, $"team-{teamId}");
+            _connectionManager.SetActiveTeam(UserId.Create(validUserId), teamId);
+
+            await Clients.Caller.ReceiveMessage($"Switched to group team-{teamId}. You have been moved from team {currentTeam ?? "none"}.");
+        }
+    }
+
+    public async Task LeaveTeamGroup(string teamId)
+    {
+        var userId = Context.User?.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+        if (userId != null && Guid.TryParse(userId, out var validUserId))
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, $"team-{teamId}");
+            await Clients.Caller.ReceiveMessage($"Leaved team group successfully. Group name: team-{teamId}. User ID: {validUserId}");
+        }
+    }
+    
     public override async Task OnConnectedAsync()
     {
         var userId = Context.User?.FindFirst(ClaimTypes.NameIdentifier)?.Value;
@@ -31,7 +65,7 @@ public class CurrencyHub : Hub<ICurrencyClient>
         if (userId != null && Guid.TryParse(userId, out var validUserId))
         {
             _connectionManager.AddConnection(UserId.Create(validUserId), Context.ConnectionId);
-            await Clients.Caller.ReceiveMessage($"Connected successfully. You are now receiving currency updates. User ID: {userId}");
+            await Clients.Caller.ReceiveMessage($"Connected successfully. You are now receiving currency updates. User ID: {validUserId}");
         }
     }
     
