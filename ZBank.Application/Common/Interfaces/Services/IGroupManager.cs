9d32746698c28ddcce0025cd89d647ee8aac437a
skmkqw <tkorsakov77@gmail.com>
2025-03-04 15:05:22 +0100

    add IGroupManager

---
+++ ZBank.Application/Common/Interfaces/Services/IGroupManager.cs
@@ -0,0 +1,18 @@
+using ZBank.Domain.UserAggregate.ValueObjects;
+
+namespace ZBank.Application.Common.Interfaces.Services;
+
+public interface IGroupManager
+{
+    void AddUserToGroup(UserId userId, string connectionId, string groupId);
+
+    void RemoveUserFromGroup(UserId userId, string connectionId, string groupId);
+
+    List<string>? GetConnectionsInGroup(string groupId);
+
+    List<UserId>? GetUsersInGroup(string groupId);
+
+    List<string> GetAllGroups();
+
+    bool IsUserInGroup(UserId userId, string groupId);
+}
