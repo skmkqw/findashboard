2b2fe6e949d6ab29abe9517e660904300a5eb0b9
skmkqw <tkorsakov77@gmail.com>
2025-03-04 22:12:15 +0100

    implemented GetTeamCurrenciesAsync method in CurrencyRepository

--- ZBank.Infrastructure/Persistance/Repositories/CurrencyRepository.cs
+++ ZBank.Infrastructure/Persistance/Repositories/CurrencyRepository.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using ZBank.Application.Common.Interfaces.Persistance;
 using ZBank.Domain.CurrencyAggregate;
 using ZBank.Domain.CurrencyAggregate.ValueObjects;
+using ZBank.Domain.TeamAggregate.ValueObjects;
 
 namespace ZBank.Infrastructure.Persistance.Repositories;
 
@@ -19,10 +20,19 @@ public class CurrencyRepository : ICurrencyRepository
     {
         return await _dbContext.Currencies.FindAsync(symbol);
     }
-
-    public async Task<List<Currency>> GetAllCurrenciesAsync()
+    
+    public async Task<List<Currency>> GetTeamCurrenciesAsync(TeamId teamId)
     {
-        return await _dbContext.Currencies.ToListAsync();
+        return await _dbContext.Currencies
+            .FromSqlRaw(@"
+                SELECT DISTINCT c.*
+                FROM ""Currencies"" c
+                INNER JOIN ""WalletBalances"" wb ON c.""Id"" = wb.""CurrencyId""
+                INNER JOIN ""Wallets"" w ON wb.""WalletId"" = w.""Id""
+                INNER JOIN ""Profiles"" p ON w.""ProfileId"" = p.""Id""
+                INNER JOIN ""Teams"" t ON p.""TeamId"" = t.""Id""
+                WHERE t.""Id"" = {0}", teamId.Value)
+            .ToListAsync();
     }
 
     public async Task ReplaceAllCurrenciesAsync(List<Currency> newCurrencies)
