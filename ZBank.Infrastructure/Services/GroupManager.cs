d9f33784d2ca43bde9ad93232ed26ebb1b559899
skmkqw <tkorsakov77@gmail.com>
2025-04-07 21:13:32 +0200

    add validation logic & implement TryAddToGroupAsync in GroupManager

--- ZBank.Infrastructure/Services/GroupManager.cs
+++ ZBank.Infrastructure/Services/GroupManager.cs
@@ -1,27 +1,39 @@
 using System.Collections.Concurrent;
+using ErrorOr;
+using ZBank.Application.Common.Interfaces.Persistance;
 using ZBank.Application.Common.Interfaces.Services;
+using ZBank.Domain.Common.Errors;
+using ZBank.Domain.TeamAggregate.ValueObjects;
 using ZBank.Domain.UserAggregate.ValueObjects;
 
 namespace ZBank.Infrastructure.Services;
 
 public class GroupManager : IGroupManager
 {
+    private readonly IUserRepository _userRepository;
+    
+    private readonly ITeamRepository _teamRepository;
+    
     private readonly ConcurrentDictionary<string, ConcurrentDictionary<UserId, HashSet<string>>> _groups = new();
 
-    public void AddUserToGroup(UserId userId, string connectionId, string groupId)
+    public GroupManager(ITeamRepository teamRepository, IUserRepository userRepository)
     {
-        var userConnections = _groups.GetOrAdd(groupId, _ => new ConcurrentDictionary<UserId, HashSet<string>>());
-        var connections = userConnections.GetOrAdd(userId, _ => new HashSet<string>());
-        
-        lock (connections)
-        {
-            connections.Add(connectionId);
-        }
+        _teamRepository = teamRepository;
+        _userRepository = userRepository;
     }
     
+    public async Task<ErrorOr<Success>> TryAddUserToGroupAsync(UserId userId, string connectionId, string groupId)
+    {
+        var validation = await ValidateGroupAsync(userId, groupId);
+        if (validation.IsError) return validation;
+
+        AddUserToGroup(userId, connectionId, groupId);
+        return Result.Success;
+    }
+
     public void RemoveUserFromGroup(UserId userId, string connectionId, string groupId)
     {
-        if (_groups.TryGetValue(groupId, out var userConnections) && 
+        if (_groups.TryGetValue(groupId, out var userConnections) &&
             userConnections.TryGetValue(userId, out var connections))
         {
             lock (connections)
@@ -39,25 +51,27 @@ public class GroupManager : IGroupManager
             }
         }
     }
-    
+
     public List<string>? GetConnectionsInGroup(string groupId)
     {
         if (_groups.TryGetValue(groupId, out var userConnections))
         {
             return userConnections.Values.SelectMany(connections => connections).ToList();
         }
+
         return null;
     }
-    
+
     public List<UserId>? GetUsersInGroup(string groupId)
     {
         if (_groups.TryGetValue(groupId, out var userConnections))
         {
             return userConnections.Keys.ToList();
         }
+
         return null;
     }
-    
+
     public List<string> GetAllGroups()
     {
         return _groups.Keys.ToList();
@@ -66,5 +80,45 @@ public class GroupManager : IGroupManager
     public bool IsUserInGroup(UserId userId, string groupId)
     {
         return _groups.TryGetValue(groupId, out var userConnections) && userConnections.ContainsKey(userId);
+    }
+    
+    private void AddUserToGroup(UserId userId, string connectionId, string groupId)
+    {
+        var userConnections = _groups.GetOrAdd(groupId, _ => new ConcurrentDictionary<UserId, HashSet<string>>());
+        var connections = userConnections.GetOrAdd(userId, _ => new HashSet<string>());
+
+        lock (connections)
+        {
+            connections.Add(connectionId);
+        }
+    }
+    
+    private async Task<ErrorOr<Success>> ValidateGroupAsync(UserId userId, string groupId)
+    {
+        if (Guid.TryParse(groupId, out var validGroupId))
+        {
+            var teamId = TeamId.Create(validGroupId);
+            
+            var user = await _userRepository.FindByIdAsync(userId);
+            if (user is null)
+            {
+                return Errors.User.IdNotFound(userId);
+            }
+            
+            var teamValidationDetails = await _teamRepository.GetTeamValidationDetailsAsync(teamId, user);
+            if (teamValidationDetails is null)
+            {
+                return Error.NotFound("Group.NotFound", "Team or personal space with given id not found does not exist");
+            }
+
+            if (!teamValidationDetails.HasAccess)
+            {
+                return Errors.Team.AccessDenied;
+            }
+            
+            return Result.Success;
+        }
+
+        return Error.Validation("Group.InvalidIdFormat", "Group id has an invalid format");
     }
 }
