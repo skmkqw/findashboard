16f0f9bab014dab97ce2d2a27c77fdeda1ea8332
skmkqw <tkorsakov77@gmail.com>
2025-04-08 12:26:19 +0200

    add GetUserTeams endpoint

--- ZBank.API/Controllers/UserController.cs
+++ ZBank.API/Controllers/UserController.cs
@@ -2,7 +2,9 @@ using MapsterMapper;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using ZBank.Application.Notifications.Queries.GetUserNotifications;
+using ZBank.Application.Users.Queries.GetUserTeams;
 using ZBank.Contracts.Notifications.GetUserNotifications;
+using ZBank.Contracts.Teams.GetTeams;
 
 namespace ZBank.API.Controllers;
 
@@ -42,5 +44,29 @@ public class UserController : ApiController
         _logger.LogInformation("Successfully fetched notifications for user with id: {UserId}", userId);
         
         return Ok(_mapper.Map<GetUserNotificationsResponse>(getUserNotificationsResult.Value));
+    }
+
+    [HttpGet("teams")]
+    public async Task<IActionResult> GetUserTeams()
+    {
+        var userId = GetUserId();
+        if (!userId.HasValue)
+        {
+            return UnauthorizedUserIdProblem();
+        }
+        
+        var query = _mapper.Map<GetUserTeamsQuery>(userId.Value);
+        
+        var getUserTeamsResult = await _mediator.Send(query);
+        
+        if (getUserTeamsResult.IsError)
+        {
+            _logger.LogInformation("Failed to fetch teams for user with id: {UserId}", userId);   
+            return Problem(getUserTeamsResult.Errors);
+        }
+        
+        _logger.LogInformation("Successfully fetched teams for user with id: {UserId}", userId);
+        
+        return Ok(_mapper.Map<GetUserTeamsResponse>(getUserTeamsResult.Value));
     }
 }
